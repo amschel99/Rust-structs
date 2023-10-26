450266d977300cee254c6855ec96bca3313f9169
amschel99 <6290.2020@students.ku.ac.ke>
2023-10-26 11:08:58 +0300

    build user function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+struct User {
+    active:bool,
+    username:String,
+    email:String,
+    sign_in_count:u64
+}
+fn main (){
+let user1=build_user(String::from("kariukiamschel9@gmail.com"),String::from("Amschel Kariuki"));
+println!("The user is called {}",user1.username);
+}
+fn build_user(email:String, username:String)->User{
+    User{
+        active:true,
+        username,
+        email,
+        sign_in_count:1
+
+    }
+
 }
